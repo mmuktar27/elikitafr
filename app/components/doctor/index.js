cfda53b5eaa337aa4cf3bb1538ddccfe4f6216d6
Muhammed Muktar Muhammed <9youngmoney@gmail.com>
2024-12-03 16:50:49 +0100

    updated referrals

--- app/components/doctor/index.js
+++ app/components/doctor/index.js
@@ -3,7 +3,7 @@ export { default as EventsPage } from "./Events";
 export { default as PatientsPage } from "./Patients";
 export { default as SettingsPage } from "./Settings";
 export { default as DashboardPage } from "./Dashboard";
-export { default as ReferralsPage } from "./referrals";
+export { default as ReferralsPage } from "./Referrals";
 export { default as PatientDetailsView } from "./PatientDetailsView";
 export { default as SmartConsultationPage } from "./SmartConsultationPage"; 
 export { default as HealthTips } from "./HealthTips"; 
